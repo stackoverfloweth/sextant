a4708fa43f4c893355ad3758c40ee2564f6478b6
Evan Sutherland <stackoverfloweth@gmail.com>
2018-05-31 21:54:05 -0500

    added next buttons to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,25 @@ import React from 'react';
 export default class Navbar extends React.Component {
     render() {
         return (
-            <nav className="navbar navbar-light bg-light">
+            <nav className="navbar navbar-light bg-light navbar-expand-lg justify-content-between">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="">
                         <span className="logo"></span>
                         <h1 className="p-2">Sextant</h1>
                     </a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link" href="">Add Jira Ticket</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="">Settings</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         )
